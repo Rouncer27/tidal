168992241f34afae5aff292fb82d466728f7b7c6
Trevor Rounce <trevor@switchbackcreative.ca>
2026-09-16 14:50:21 -0600

    slick fix

--- src/data/podcast/queries.js
+++ src/data/podcast/queries.js
@@ -13,3 +13,25 @@ export const Podcast_Hero_Query = `
         }
     }
 `;
+
+export const Podcast_Intro_Query = `
+    podcastIntro: podcastIntroComponent {
+        content
+        buttonText
+        buttonLink
+        points {
+            title
+            content
+            image {
+                node {
+                    sourceUrl
+                    altText
+                    mediaDetails {
+                        width
+                        height
+                    }
+                }
+            }
+        }
+    }
+`;
