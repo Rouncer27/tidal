d31663c6c1b0a1eaaa6a0249153b3f1f385f7b22
Trevor Rounce <trevor@switchbackcreative.ca>
2026-09-16 09:46:06 -0600

    home page

--- src/data/home/queries.js
+++ src/data/home/queries.js
@@ -148,6 +148,7 @@ export const Meet_Us_Query = `
         linkBlocks {
             title
             content
+            buttonText
             icon  {
                 node {
                     sourceUrl
@@ -158,6 +159,7 @@ export const Meet_Us_Query = `
                     }
                 }
             }
+        
         buttonLink {
             edges {
                 node {
