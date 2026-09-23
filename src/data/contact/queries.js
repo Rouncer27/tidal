935fbc054f5a5ade6fd4dd1dc9516801dc83fbe1
Trevor Rounce <trevor@switchbackcreative.ca>
2026-09-23 09:09:28 -0600

    contact page meet us

--- src/data/contact/queries.js
+++ src/data/contact/queries.js
@@ -13,3 +13,43 @@ export const Contact_Hero_Query = `
         }
     }
 `;
+
+export const Meet_Us_Query = `
+    meetUs: meetUsComponent {
+        topTitle
+        title
+        image  {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                 }
+            }
+        }
+        linkBlocks {
+            title
+            content
+            buttonText
+            icon  {
+                node {
+                    sourceUrl
+                    altText
+                    mediaDetails {
+                        width
+                        height
+                    }
+                }
+            }
+        
+        buttonLink {
+            edges {
+                node {
+                    uri
+                    }
+                }
+            }
+        }
+    }
+`;
